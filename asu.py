4043e43a7db52a1b3fc058631e6f1bfb91ca859f
Abel van Hulst <abelvanhulst@gmail.com>
2026-06-05 16:30:50 +0200

    asu_unlimited.txt

--- asu.py
+++ asu.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-file_path = os.path.join("data", "asu.txt")
+file_path = os.path.join("data", "asu_unlimited.txt")
 
 data = np.genfromtxt(file_path, delimiter=";", usecols=(1, 2, 3), dtype=np.float32, filling_values=np.nan)
 
@@ -16,11 +16,11 @@ bv_index = data_clean[:, 2]
 absolute_magnitude = apparent_magnitude - 5 * np.log10(1 / (parallex / 1000)) + 5
 
 plt.grid()
-plt.scatter(bv_index, absolute_magnitude)
+plt.scatter(bv_index, absolute_magnitude, s=0.1)
 plt.gca().invert_yaxis()
 plt.gca().set_axisbelow(True)
-plt.title("The Hipparcos and Tycho Catalogues (ESA 1997) - 200")
+plt.title("The Hipparcos and Tycho Catalogues (ESA 1997)")
 plt.xlabel("B-V Index")
 plt.ylabel("Absolute Magnitude")
-plt.savefig("asu-200.svg", bbox_inches='tight')
-plt.savefig("asu-200.png", bbox_inches='tight')
+plt.savefig("asu_unlimited.svg", bbox_inches='tight')
+plt.savefig("asu_unlimited.png", bbox_inches='tight')
